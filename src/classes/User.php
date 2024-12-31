82ea1240884f68833856c3a21b1f207c89c86ea9
ouahmane youness <youness.ouahmane4@gmail.com>
2024-12-31 19:46:33 +0100

    starting user class

---
+++ src/classes/User.php
@@ -0,0 +1,16 @@
+<?php
+
+class user{
+    protected $name;
+    protected $phone_number;
+    protected $email_adress;
+    protected $password;
+    protected $role;
+
+
+    public function __construct($name, $phone, $email, $password, $role)
+    {
+        
+    }
+
+}
