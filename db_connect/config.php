221a0c8b984a6ecbea627314e1d602938d2a5158
ouahmane youness <youness.ouahmane4@gmail.com>
2024-12-31 16:15:35 +0100

    db_connect

---
+++ db_connect/config.php
@@ -0,0 +1,24 @@
+<?php
+
+class database
+{
+    private $host = "localhost";
+    private $user_name = "root";
+    private $password = "";
+    private $db_name = "career_link";
+    private $conn;
+
+    public function connect()
+    {
+        try {
+            $this->conn = null;
+            $this->conn = new pdo("mysql:host" . $this->host . ";dbname=" . $this->db_name, $this->user_name, $this->password);
+
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
