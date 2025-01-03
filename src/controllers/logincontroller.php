ab6fcd8a0935fee240c318462f895692d112ade1
ouahmane youness <youness.ouahmane4@gmail.com>
2025-01-03 20:38:59 +0100

    add index

--- src/controllers/logincontroller.php
+++ src/controllers/logincontroller.php
@@ -1,23 +1,29 @@
 <?php
 
+session_start();
+
+$error_message = [];
+
 if(isset($_POST["submit"]))
 {
     $email = $_POST["email"];
     $password = $_POST["password"];
 
 
-    if (empty($email)) {
-        $error_message['email'] = "Please fill all fields.";
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $error_message['email'] = "Please enter a valid email address.";
+    if (empty($email) || empty($password)) {
+        $_SESSION['error_message'][] = "Please fill all fields.";
+        header('Location: ../views/login.php');
+        exit();
     }
 
-    if (empty($password)) {
-        $error_message['password'] = "Password is required.";
-    } elseif (strlen($password) < 6) {
-        $error_message['password'] = "Password is incorrect.";
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $_SESSION['emailerror'] = "Please enter a valid email address.";
     }
+
+
 
 
 
 }
+
+
